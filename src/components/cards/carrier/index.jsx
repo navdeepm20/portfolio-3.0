5619d4a8d9cbe0c13ae174ee1a5de4a92f766aca
navdeepm20 <navdeepmishra82734@gmail.com>
2024-01-11 23:37:46 +0530

    fixed date issues in exp cards, other minor fixes and updates

--- src/components/cards/carrier/index.jsx
+++ src/components/cards/carrier/index.jsx
@@ -3,8 +3,10 @@ import React, { forwardRef } from "react";
 import dayjs from "dayjs";
 import * as relativeTime from "dayjs/plugin/relativeTime";
 import * as customParseFormat from "dayjs/plugin/customParseFormat";
+import * as duration from "dayjs/plugin/duration";
 dayjs.extend(relativeTime);
 dayjs.extend(customParseFormat);
+dayjs.extend(duration);
 
 const CarrierCard = forwardRef(
   (
@@ -19,6 +21,29 @@ const CarrierCard = forwardRef(
     },
     ref
   ) => {
+    const getCalculatedDuration = () => {
+      const startedOnDate = dayjs(startedOn);
+      const endedOnDate = endedOn ? dayjs(endedOn) : dayjs();
+
+      const years = endedOnDate.diff(startedOnDate, "year");
+      const months = endedOnDate.diff(
+        startedOnDate.add(years, "year"),
+        "month"
+      );
+
+      let formattedDuration = "";
+      if (years)
+        years > 1
+          ? (formattedDuration += `${years} yrs `)
+          : (formattedDuration += `${years} yr `);
+      if (months)
+        months > 1
+          ? (formattedDuration += `${months} months`)
+          : (formattedDuration += `${months} month`);
+
+      return formattedDuration;
+    };
+
     return (
       <div
         className="carrier-card rounded-[1.6rem] border border-primary-bg-light p-[3rem] max-w-[58rem] translate-y-[30%] opacity-0 transition-all duration-500 hover:shadow-[0_0_10px_2px] hover:shadow-[rgba(51,48,51,1)] "
@@ -32,8 +57,12 @@ const CarrierCard = forwardRef(
         </div>
         <div className="carrier-card__timeline-container flex">
           {!hideTillToday && (
-            <span className="mr-auto font-bold text-[1.8rem] leading-[2.1rem] text-primary-bg-light">
-              {dayjs(new Date()).from(startedOn, true)}
+            <span className="mr-auto font-bold text-[1.8rem] leading-[2.1rem] text-primary-bg-light pr-2">
+              {/* {dayjs(endedOn ? new Date(endedOn) : new Date()).from(
+                startedOn,
+                true
+              )} */}
+              {getCalculatedDuration()}
             </span>
           )}
           <span className="ml-auto text-[1.8rem] leading-[2.1rem] text-primary-bg-light">
--- src/components/sections/hero/index.jsx
+++ src/components/sections/hero/index.jsx
@@ -21,22 +21,34 @@ function index({ ...props }) {
     >
       <div className="hero-section__personal-info order-2 md:order-1">
         <Badge icon={"👋"} text="Greetings!" tailwindStyles="mb-[2.4rem]" />
-        <p className="text-white text-[5.2rem] leading-[6.1rem] font-extrabold mb-[1.6rem]">
+        <p className="text-white text-[5.2rem] leading-[6.1rem] font-extrabold mb-[1.6rem] animate-character">
           {data?.personal?.data?.name}
         </p>
-        <p className="text-[2rem] leading-[2.3rem] text-customGrey mb-[2.476rem]">
-          {data?.personal?.data?.designationPrimary} &middot;{" "}
-          {data?.personal?.data?.designationSecondary}
+        <p className="text-[2rem] leading-[2.3rem] text-customGrey mb-[2.476rem] ">
+          {data?.personal?.data?.designation?.join(" · ")}
         </p>
         <div className="hero-section__personal-info-social-icons  gap-[1.5rem] hidden sm:flex ">
           <Link href={`${data?.personal?.data?.githubLink}`}>
-            <Image src={GithubLogo} alt="github_logo" />
+            <Image
+              src={GithubLogo}
+              alt="github_logo"
+              className="hover:drop-shadow-glow transition-all"
+            />
           </Link>
           <Link href={`${data?.personal?.data?.linkedInLink}`}>
-            <Image src={LinkedInLogo} alt="linkedIn_logo" />
+            <Image
+              src={LinkedInLogo}
+              alt="linkedIn_logo"
+              className="hover:drop-shadow-glow transition-all"
+            />
           </Link>
           <Link href={`${data?.personal?.data?.devLink}`}>
-            <Image src={DevLogo} alt="linkedIn_logo" width={32} />
+            <Image
+              src={DevLogo}
+              alt="linkedIn_logo"
+              width={32}
+              className="hover:drop-shadow-glow transition-all"
+            />
           </Link>
         </div>
       </div>
